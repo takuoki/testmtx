5ca5503b66837e105a3e7e4668b08972c27fafb0
takuo.oki <takuo.oki@optim.co.jp>
2023-04-09 19:24:07 +0900

    implement outputter

--- v2/output.go
+++ v2/output.go
@@ -1,6 +1,7 @@
 package testmtx
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"os"
@@ -8,10 +9,6 @@ import (
 
 	"golang.org/x/sync/errgroup"
 )
-
-// TODO
-// - 出力するディレクトリ構成等を考慮し、適切にファイルを作成する
-// - ファイル自体はformatterに任せる
 
 type Outputter interface {
 	Output(baseDir string, sheet *Sheet) error
@@ -34,7 +31,65 @@ func (o *oneColumnOneCaseOutputter) Output(baseDir string, sheet *Sheet) error {
 	}
 
 	for propName, col := range sheet.Collections {
-		dir := filepath.Join(baseDir, string(propName))
+		eg := &errgroup.Group{}
+		for _, cn := range sheet.ColumnNames {
+			cn := cn
+			eg.Go(func() (er error) {
+				defer func() {
+					if e := recover(); e != nil {
+						er = fmt.Errorf("panic recovered in goroutine: %v", e)
+					}
+				}()
+
+				buf := &bytes.Buffer{}
+				o.formatter.Write(buf, col, cn, 0)
+				if buf.String() == "" {
+					return nil
+				}
+
+				dir := filepath.Join(baseDir, sheet.Name, string(cn))
+				if err := os.MkdirAll(dir, 0777); err != nil {
+					return fmt.Errorf("fail to create directory: %w", err)
+				}
+
+				fp := filepath.Join(dir, fmt.Sprintf("%s.%s", propName, o.formatter.Extension()))
+				file, err := os.Create(fp)
+				if err != nil {
+					return fmt.Errorf("fail to create file: %w", err)
+				}
+				defer file.Close()
+
+				fmt.Fprint(file, buf.String())
+
+				return nil
+			})
+		}
+		if err := eg.Wait(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type oneSheetOneCaseOutputter struct {
+	formatter Formatter
+}
+
+func NewOneSheetOneCaseOutputter(formatter Formatter) Outputter {
+	return &oneSheetOneCaseOutputter{
+		formatter: formatter,
+	}
+}
+
+func (o *oneSheetOneCaseOutputter) Output(baseDir string, sheet *Sheet) error {
+
+	if sheet == nil {
+		return errors.New("sheet is nil")
+	}
+
+	for propName, col := range sheet.Collections {
+		dir := filepath.Join(baseDir, sheet.Name, string(propName))
 		if err := os.MkdirAll(dir, 0777); err != nil {
 			return fmt.Errorf("fail to create directory: %w", err)
 		}
@@ -48,15 +103,20 @@ func (o *oneColumnOneCaseOutputter) Output(baseDir string, sheet *Sheet) error {
 					}
 				}()
 
-				fp := filepath.Join(dir, fmt.Sprintf("%s_%s.%s", sheet.Name, cn, o.formatter.Extension()))
+				buf := &bytes.Buffer{}
+				o.formatter.Write(buf, col, cn, 0)
+				if buf.String() == "" {
+					return nil
+				}
 
+				fp := filepath.Join(dir, fmt.Sprintf("%s.%s", cn, o.formatter.Extension()))
 				file, err := os.Create(fp)
 				if err != nil {
 					return fmt.Errorf("fail to create file: %w", err)
 				}
 				defer file.Close()
 
-				o.formatter.Write(file, col, cn, 0)
+				fmt.Fprint(file, buf.String())
 
 				return nil
 			})
