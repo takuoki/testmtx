c097aad6eecaaf0b289819bfba85ffe19d2fcd0c
takuoki <t.oki@kakehashi.life>
2024-09-22 20:33:19 +0900

    misc

--- v2/output.go
+++ v2/output.go
@@ -14,7 +14,7 @@ type Outputter interface {
 	Output(baseDir string, sheet *Sheet) error
 }
 
-type NewOutputterFunc = func(formatter Formatter) Outputter
+type NewOutputterFunc func(formatter Formatter) Outputter
 
 type oneColumnOneCaseOutputter struct {
 	formatter Formatter
